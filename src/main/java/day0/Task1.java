0d15139e1f5e2f2d1eb60b6dc40b7b75381500ab
Arthur V <xarturx7@gmail.com>
2021-02-22 21:05:47 +0200

    Add day1 tasks solutions

--- src/main/java/day0/Task1.java
+++
@@ -1,7 +0,0 @@
-package day0;
-
-public class Task1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int i = 0;
+        while(i < 10) {
+            i++;
+            System.out.println("JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA");
+        }
     }
 }
